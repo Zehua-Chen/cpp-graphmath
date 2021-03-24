962b4ca5bf0e6388100555cdeb1882ab4588584b
Zehua-Chen <peterchen06242000@outlook.com>
2021-03-23 23:46:14 -0500

    [feat] Add More Matrix Multiplication and ToString to Float4x4

--- include/graphmath/float4x4.h
+++ include/graphmath/float4x4.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ostream>
+
 #include "graphmath/float4.h"
 #include "graphmath/not_implemented.h"
 
@@ -10,13 +12,6 @@
 // Declarations
 
 namespace graphmath {
-struct float4x4;
-
-/// @brief Transpose a `float4x4` matrix
-/// @param f4x4 the matrix to transpose
-/// @returns the transposed matrix
-float4x4 transpose(const float4x4 &f4x4);
-
 /// @brief a 4x4 matrix
 struct float4x4 final {
  public:
@@ -43,44 +38,50 @@ struct float4x4 final {
   float4x4(const float4 &row0, const float4 &row1, const float4 &row2,
            const float4 &row3);
 
-  /// @brief Get the value at `(row, column)`
-  /// @param row row
-  /// @param column column
+  /// @brief Get the value at `(x, y)`
+  /// @param y y
+  /// @param x x
   /// @returns the value at `(row, column)`
-  float get(size_t row, size_t column) const;
+  float get(size_t y, size_t x) const;
 
-  /// @brief Set the value at `(row, column)`
-  /// @param row row
-  /// @param column column
-  /// @param value the value at `(row, column)`
-  void set(size_t row, size_t column, float value);
+  /// @brief Set the value at `(x, y)`
+  /// @param y y
+  /// @param x x
+  /// @param value the value at `(x, y)`
+  void set(size_t y, size_t x, float value);
 
-  /// @brief Get the value at `(row, column)`
-  /// @param row row
-  /// @param column column
-  /// @returns the value at `(row, column)`
-  float operator()(size_t row, size_t column) const;
+  /// @brief Get the value at `(x, y)`
+  /// @param y y
+  /// @param x x
+  /// @returns the value at `(x, y)`
+  float operator()(size_t y, size_t x) const;
 
   /// @brief Multiply a `float4x4` by a `float4`
   /// @param f4 the `float4`
   /// @returns the result of multiplication
   float4 operator*(const float4 &f4) const;
 
+  /// @brief Multiply a `float4x4` by a `float4x4`
+  /// @param f4x4 the `float4x4`
+  /// @returns the result of multiplication
+  float4x4 operator*(const float4x4 &f4x4) const;
+
   native_float4x4 native;
 };
+
+/// @brief Transpose a `float4x4` matrix
+/// @param f4x4 the matrix to transpose
+/// @returns the transposed matrix
+float4x4 transpose(const float4x4 &f4x4);
+
+template <typename CharT>
+std::basic_ostream<CharT> &operator<<(std::basic_ostream<CharT> &out,
+                                      const float4x4 &f4x4);
 }  // namespace graphmath
 
 // Imlementations
 
 namespace graphmath {
-inline float4x4 transpose(const float4x4 &f4x4) {
-#if defined(__APPLE__)
-  return float4x4{simd::transpose(f4x4.native)};
-#else
-  throw_not_implemented();
-#endif
-}
-
 inline float4x4::float4x4(const native_float4x4 &native) : native(native) {}
 
 inline float4x4::float4x4(float value) {
@@ -108,24 +109,24 @@ inline float4x4::float4x4(const float4 &row0, const float4 &row1,
 #endif
 }
 
-inline float float4x4::get(size_t row, size_t column) const {
+inline float float4x4::get(size_t y, size_t x) const {
 #if defined(__APPLE__)
-  return native.columns[column][row];
+  return native.columns[x][y];
 #else
   throw_not_implemented();
 #endif
 }
 
-inline void float4x4::set(size_t row, size_t column, float value) {
+inline void float4x4::set(size_t y, size_t x, float value) {
 #if defined(__APPLE__)
-  native.columns[column][row] = value;
+  native.columns[x][y] = value;
 #else
   throw_not_implemented();
 #endif
 }
 
-inline float float4x4::operator()(size_t row, size_t column) const {
-  return get(row, column);
+inline float float4x4::operator()(size_t y, size_t x) const {
+  return get(y, x);
 }
 
 inline float4 float4x4::operator*(const float4 &f4) const {
@@ -134,5 +135,46 @@ inline float4 float4x4::operator*(const float4 &f4) const {
 #else
   throw_not_implemented();
 #endif
+}
+
+inline float4x4 float4x4::operator*(const float4x4 &f4x4) const {
+#if defined(__APPLE__)
+  return float4x4{native * f4x4.native};
+#else
+  throw_not_implemented();
+#endif
+}
+
+inline float4x4 transpose(const float4x4 &f4x4) {
+#if defined(__APPLE__)
+  return float4x4{simd::transpose(f4x4.native)};
+#else
+  throw_not_implemented();
+#endif
+}
+
+template <typename CharT>
+std::basic_ostream<CharT> &operator<<(std::basic_ostream<CharT> &out,
+                                      const float4x4 &f4x4) {
+  const CharT space = static_cast<CharT>(' ');
+  const auto print_line = [&](size_t y) {
+    out << f4x4.get(y, 0) << space;
+    out << f4x4.get(y, 1) << space;
+    out << f4x4.get(y, 2) << space;
+    out << f4x4.get(y, 3);
+  };
+
+  print_line(0);
+  out << std::endl;
+
+  print_line(1);
+  out << std::endl;
+
+  print_line(2);
+  out << std::endl;
+
+  print_line(3);
+
+  return out;
 }
 }  // namespace graphmath
--- unittests/float4x4_test.cc
+++ unittests/float4x4_test.cc
@@ -5,20 +5,15 @@
 using namespace graphmath;
 
 TEST(Float4x4, Construction) {
-  float4 rows[4];
-  float counter = 0.0f;
+  float4x4 matrix{float4{0, 1, 2, 3}, float4{4, 5, 6, 7}, float4{8, 9, 10, 11},
+                  float4{12, 13, 14, 15}};
 
-  for (size_t row = 0; row < 4; row++) {
-    rows[row] = float4{counter++, counter++, counter++, counter++};
-  }
+  float counter = 0;
 
-  float4x4 matrix{rows[0], rows[1], rows[2], rows[3]};
-
-  for (size_t row = 0; row < 4; row++) {
-    ASSERT_FLOAT_EQ(matrix.get(row, 0), rows[row].x());
-    ASSERT_FLOAT_EQ(matrix.get(row, 1), rows[row].y());
-    ASSERT_FLOAT_EQ(matrix.get(row, 2), rows[row].z());
-    ASSERT_FLOAT_EQ(matrix.get(row, 3), rows[row].w());
+  for (size_t y = 0; y < 4; y++) {
+    for (size_t x = 0; x < 4; x++) {
+      ASSERT_FLOAT_EQ(matrix.get(y, x), counter++);
+    }
   }
 }
 
@@ -34,15 +29,9 @@ TEST(Float4x4, Identity) {
   ASSERT_FLOAT_EQ(result.w(), 4);
 }
 
-TEST(Float4x4, Multiplication) {
-  float4 rows[4];
-  float counter = 0.0f;
-
-  for (size_t row = 0; row < 4; row++) {
-    rows[row] = float4{counter++, counter++, counter++, counter++};
-  }
-
-  float4x4 matrix{rows[0], rows[1], rows[2], rows[3]};
+TEST(Float4x4, MultiplicationByFloat4) {
+  float4x4 matrix{float4{0, 1, 2, 3}, float4{4, 5, 6, 7}, float4{8, 9, 10, 11},
+                  float4{12, 13, 14, 15}};
   float4 f4{1, 2, 3, 4};
   float4 result = matrix * f4;
 
@@ -50,6 +39,22 @@ TEST(Float4x4, Multiplication) {
   ASSERT_FLOAT_EQ(result.y(), 60);
   ASSERT_FLOAT_EQ(result.z(), 100);
   ASSERT_FLOAT_EQ(result.w(), 140);
+}
+
+TEST(Float4x4, MultiplicationByFloat4x4) {
+  float4x4 matrix{float4{0, 1, 2, 3}, float4{4, 5, 6, 7}, float4{8, 9, 10, 11},
+                  float4{12, 13, 14, 15}};
+
+  float4x4 result = matrix * matrix;
+
+  float4x4 expected{float4{56, 62, 68, 74}, float4{152, 174, 196, 218},
+                    float4{248, 286, 324, 362}, float4{344, 398, 452, 506}};
+
+  for (size_t y = 0; y < 4; y++) {
+    for (size_t x = 0; x < 4; x++) {
+      ASSERT_FLOAT_EQ(result.get(y, x), expected.get(y, x));
+    }
+  }
 }
 
 TEST(Float4x4, Transpose) {
